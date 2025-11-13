a487b50fc27ffe55000b3be18cdd7745bdbc20a0
Saket1008 <casaketjain279@gmail.com>
2025-11-13 19:19:07 +0530

    Add comment marker to functions/index.js
    Appended a comment '//AAYUSH' to the end of the file. No functional changes made.

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
 // Deprecated: This folder is unused. Functions source is set to "server" in firebase.json.
 // Keeping this stub to avoid confusion; do not deploy from here.
 export const api = undefined;
+
+//AAYUSH
